6f539f0d1a116d73a7685c0caf3f1760d75b086a
sadicangel <sadicangel@hotmail.com>
2023-09-17 01:11:08 +0100

    Adds Stream extensions to insert and remove data.

---
+++ src/Lionware/IO/StreamExtensions.cs
@@ -0,0 +1,115 @@
+﻿using System.Buffers;
+
+namespace Lionware.IO;
+
+/// <summary>
+/// Extensions for <see cref="Stream" />.
+/// </summary>s
+public static class StreamExtensions
+{
+    /// <summary>
+    /// Inserts data in the stream at <paramref name="offset" />.
+    /// </summary>
+    /// <param name="stream">The stream to insert data to.</param>
+    /// <param name="offset">The position in the stream where the first byte of <paramref name="data"/> is written.</param>
+    /// <param name="data">The bytes to insert.</param>
+    public static void InsertRange(this Stream stream, long offset, ReadOnlySpan<byte> data)
+    {
+        Ensure.LessThanOrEqualTo(offset, stream.Length);
+
+        // Don't do anything if data is empty.
+        if (data.Length == 0)
+            return;
+
+        if (offset < stream.Length)
+        {
+            const int maxBufferSize = 1024;
+            var bytesToCopy = stream.Length - offset;
+
+            var bufferSize = (int)Math.Min(bytesToCopy, maxBufferSize);
+
+            var array = ArrayPool<byte>.Shared.Rent(bufferSize);
+            var buffer = array.AsSpan(0, bufferSize);
+
+            int bytesRead;
+            for (var i = stream.Length; i > offset; i -= bytesRead)
+            {
+                // Read block from the end.
+                var bytesToRead = (int)Math.Min(bytesToCopy, buffer.Length);
+                var position = i - bytesToRead;
+                stream.Position = position;
+                bytesRead = 0;
+                while (bytesToRead > 0)
+                {
+                    bytesRead = stream.Read(buffer.Slice(bytesRead, bytesToRead));
+                    bytesToRead -= bytesRead;
+                }
+                bytesToCopy -= bytesRead;
+                // Write at block start + data offset.
+                stream.Position = position + data.Length;
+                stream.Write(buffer[..bytesRead]);
+            }
+            ArrayPool<byte>.Shared.Return(array);
+        }
+
+        // Insert the data.
+        stream.Position = offset;
+        stream.Write(data);
+    }
+
+    /// <summary>
+    /// Removes data from the stream.
+    /// </summary>
+    /// <param name="stream">The stream to remove data from.</param>
+    /// <param name="range">The range to remove from the stream.</param>
+    public static void RemoveRange(this Stream stream, Range range)
+    {
+        var (offset, length) = range.GetOffsetAndLength(checked((int)stream.Length));
+        stream.RemoveRange(offset, length);
+    }
+
+    /// <summary>
+    /// Removes data from the stream, starting from 
+    /// <paramref name="offset" /> up to <paramref name="length" /> bytes.
+    /// </summary>
+    /// <param name="stream">The stream to remove data from.</param>
+    /// <param name="offset">The start position.</param>
+    /// <param name="length">The number of bytes to remove.</param>
+    public static void RemoveRange(this Stream stream, long offset, long length)
+    {
+        Ensure.LessThanOrEqualTo(offset + length, stream.Length);
+
+        // Don't do anything if we're already at EOS.
+        if (stream.Length == offset)
+            return;
+
+        const int maxBufferSize = 1024;
+        var bytesToCopy = stream.Length - offset - length;
+
+        var bufferSize = (int)Math.Min(bytesToCopy, maxBufferSize);
+
+        var array = ArrayPool<byte>.Shared.Rent(bufferSize);
+        var buffer = array.AsSpan(0, bufferSize);
+
+        int bytesRead;
+        for (var i = offset + length; i < stream.Length; i += bytesRead)
+        {
+            // Read block from current position.
+            var bytesToRead = (int)Math.Min(bytesToCopy, buffer.Length);
+            stream.Position = i;
+            bytesRead = 0;
+            while (bytesToRead > 0)
+            {
+                bytesRead = stream.Read(buffer.Slice(bytesRead, bytesToRead));
+                bytesToRead -= bytesRead;
+            }
+            bytesToCopy -= bytesRead;
+            // Write at block i - length offset.
+            stream.Position = i - length;
+            stream.Write(buffer[..bytesRead]);
+        }
+
+        ArrayPool<byte>.Shared.Return(array);
+        stream.SetLength(stream.Length - length);
+    }
+}
---
+++ tests/Lionware.Tests/IO/StreamExtensionsTests.cs
@@ -0,0 +1,196 @@
+﻿namespace Lionware.IO;
+
+/// <summary>
+/// Extensions for <see cref="Stream" />.
+/// </summary>s
+public sealed class StreamExtensionsTests
+{
+    [Fact]
+    public void Stream_InsertRange_InsertsBytesAtStart()
+    {
+        using var stream = new MemoryStream();
+        stream.Write(stackalloc byte[8] { 2, 3, 4, 5, 6, 7, 8, 9 });
+
+        stream.InsertRange(0, stackalloc byte[2] { 0, 1 });
+
+        var expected = new byte[10] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }.AsSpan();
+        var actual = stream.ToArray().AsSpan();
+
+        Assert.True(expected.SequenceEqual(actual));
+    }
+
+    [Fact]
+    public void Stream_InsertRange_InsertsBytesInMiddle()
+    {
+        using var stream = new MemoryStream();
+        stream.Write(stackalloc byte[8] { 0, 1, 2, 3, 6, 7, 8, 9 });
+
+        stream.InsertRange(4, stackalloc byte[2] { 4, 5 });
+
+        var expected = new byte[10] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }.AsSpan();
+        var actual = stream.ToArray().AsSpan();
+
+        Assert.True(expected.SequenceEqual(actual));
+    }
+
+    [Fact]
+    public void Stream_InsertRange_InsertsBytesInMiddle_Big()
+    {
+        const int size = 8 * 1234;
+
+        using var stream = new MemoryStream();
+        var pattern = Enumerable.Range(0, 128).Select(i => (byte)i).ToArray();
+        for (int k = 0; k < size;)
+        {
+            var bytesToWrite = Math.Min(pattern.Length, size - k);
+            stream.Write(pattern.AsSpan(0, bytesToWrite));
+            k += bytesToWrite;
+        }
+
+        const int offset = 128 * 10;
+
+        stream.InsertRange(offset, Enumerable.Range(128, 128).Select(i => (byte)i).ToArray());
+
+        var result = stream.ToArray();
+
+        var i = 0;
+        for (; i < offset; ++i)
+            Assert.Equal(i % 128, result[i]);
+
+        for (var j = 0; i < offset + 128; ++i, ++j)
+            Assert.Equal(128 + j, result[i]);
+
+        for (; i < result.Length; ++i)
+            Assert.Equal(i % 128, result[i]);
+    }
+
+    [Fact]
+    public void Stream_InsertRange_InsertsBytesAtEnd()
+    {
+        using var stream = new MemoryStream();
+        stream.Write(stackalloc byte[8] { 0, 1, 2, 3, 4, 5, 6, 7 });
+
+        stream.InsertRange(8, stackalloc byte[2] { 8, 9 });
+
+        var expected = new byte[10] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }.AsSpan();
+        var actual = stream.ToArray().AsSpan();
+
+        Assert.True(expected.SequenceEqual(actual));
+    }
+
+    [Fact]
+    public void Stream_InsertRange_ThrowsIfStartAfterEnd()
+    {
+        using var stream = new MemoryStream();
+
+        Assert.Throws<ArgumentOutOfRangeException>(() => stream.InsertRange(1, stackalloc byte[0]));
+    }
+
+    [Fact]
+    public void Stream_RemoveRange_RemovesBytesAtStart()
+    {
+        using var stream = new MemoryStream(Enumerable.Range(0, 10).Select(i => (byte)i).ToArray());
+
+        stream.RemoveRange(..2);
+
+        var expected = new byte[8] { 2, 3, 4, 5, 6, 7, 8, 9 }.AsSpan();
+        var actual = stream.ToArray().AsSpan();
+
+        Assert.True(expected.SequenceEqual(actual));
+    }
+
+    [Fact]
+    public void Stream_RemoveRange_RemovesBytesInMiddle()
+    {
+        using var stream = new MemoryStream(Enumerable.Range(0, 10).Select(i => (byte)i).ToArray());
+
+        stream.RemoveRange(4..6);
+
+        var expected = new byte[8] { 0, 1, 2, 3, 6, 7, 8, 9 }.AsSpan();
+        var actual = stream.ToArray().AsSpan();
+
+        Assert.True(expected.SequenceEqual(actual));
+    }
+
+    [Fact]
+    public void Stream_RemoveRange_RemovesBytesInMiddle_Big()
+    {
+        const int size = 8 * 1234;
+        const int offset = 128 * 10;
+
+        using var stream = new MemoryStream();
+        int i = 0;
+        for (; i < offset; ++i)
+            stream.WriteByte((byte)(i % 128));
+
+        for (var j = 0; i < offset + 128; ++i, ++j)
+            stream.WriteByte((byte)(j + 128));
+
+        for (; i < size; ++i)
+            stream.WriteByte((byte)(i % 128));
+
+        stream.RemoveRange(offset, 128);
+
+        var result = stream.ToArray();
+        for (i = 0; i < result.Length; i++)
+            Assert.Equal(i % 128, result[i]);
+    }
+
+    [Fact]
+    public void Stream_RemoveRange_RemovesBytesAtEnd()
+    {
+        using var stream = new MemoryStream(Enumerable.Range(0, 10).Select(i => (byte)i).ToArray());
+
+        stream.RemoveRange(^2..);
+
+        var expected = new byte[8] { 0, 1, 2, 3, 4, 5, 6, 7 }.AsSpan();
+        var actual = stream.ToArray().AsSpan();
+
+        Assert.True(expected.SequenceEqual(actual));
+    }
+
+    [Fact]
+    public void Stream_RemoveRange_IgnoresIfRangeAfterEnd()
+    {
+        using var stream = new MemoryStream(Enumerable.Range(0, 10).Select(i => (byte)i).ToArray());
+
+        stream.RemoveRange(10..);
+
+        var expected = new byte[10] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }.AsSpan();
+        var actual = stream.ToArray().AsSpan();
+
+        Assert.True(expected.SequenceEqual(actual));
+    }
+
+    [Fact]
+    public void Stream_RemoveRange_ThrowsIfStartAfterEnd()
+    {
+        using var stream = new MemoryStream();
+
+        Assert.Throws<ArgumentOutOfRangeException>(() => stream.RemoveRange(1, 1));
+    }
+
+    [Fact]
+    public void Stream_RemoveRange_ThrowsIfRangeAfterEnd()
+    {
+        using var stream = new MemoryStream();
+
+        Assert.Throws<ArgumentOutOfRangeException>(() => stream.RemoveRange(1..));
+    }
+
+    [Fact]
+    public void Stream_RemoveRange_ThrowsIfBytesLengthAfterEnd()
+    {
+        using var stream = new MemoryStream();
+
+        Assert.Throws<ArgumentOutOfRangeException>(() => stream.RemoveRange(0, 5));
+    }
+
+    [Fact]
+    public void Stream_RemoveRange_ThrowsIfRangeLengthAfterEnd()
+    {
+        using var stream = new MemoryStream();
+
+        Assert.Throws<ArgumentOutOfRangeException>(() => stream.RemoveRange(..5));
+    }
+}
